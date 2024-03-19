1610e56cd350b70fae1a7500f6a3a9835d44bf5f
pejman <pejman.hkh@gmail.com>
2024-03-19 18:01:03 +0330

    add RouteHandle to handle routes

--- main.go
+++ main.go
@@ -3,21 +3,38 @@ package main
 import (
 	"goreact/app/controller"
 	"net/http"
+	"reflect"
 
 	"github.com/gin-gonic/gin"
 )
 
+func Invoke(obj any, name string, args ...any) []reflect.Value {
+	inputs := make([]reflect.Value, len(args))
+	for i, _ := range args {
+		inputs[i] = reflect.ValueOf(args[i])
+	}
+
+	return reflect.ValueOf(obj).MethodByName(name).Call(inputs)
+}
+
+func RouteHandle(ctx *gin.Context, obj any, method string) {
+	rf := Invoke(obj, method, ctx)
+	res := rf[0].Interface().(string)
+
+	ctx.Data(http.StatusOK, "text/html; charset=utf-8", []byte(res))
+}
+
 func main() {
 
 	r := gin.Default()
+	index := &controller.IndexController{}
+	user := &controller.UserController{}
 	r.GET("/", func(ctx *gin.Context) {
-		index := controller.IndexController{}
-		ctx.Data(http.StatusOK, "text/html; charset=utf-8", []byte(index.Index(ctx)))
+		RouteHandle(ctx, index, "Index")
 	})
 
 	r.GET("/login", func(ctx *gin.Context) {
-		user := controller.UserController{}
-		ctx.Data(http.StatusOK, "text/html; charset=utf-8", []byte(user.Login(ctx)))
+		RouteHandle(ctx, user, "Login")
 	})
 
 	r.POST("/login", func(ctx *gin.Context) {
