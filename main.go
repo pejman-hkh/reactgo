d6caab5d60d8cfaec5a5570c187f91d825f58a17
pejman <pejman.hkh@gmail.com>
2024-03-19 20:06:31 +0330

    add cache for rendering, in the next i should add some state for clear cache

--- main.go
+++ main.go
@@ -1,45 +1,30 @@
 package main
 
 import (
+	"fmt"
 	"goreact/app/controller"
-	"net/http"
-	"reflect"
+	goreact "goreact/src"
 
 	"github.com/gin-gonic/gin"
 )
 
-func Invoke(obj any, name string, args ...any) []reflect.Value {
-	inputs := make([]reflect.Value, len(args))
-	for i, _ := range args {
-		inputs[i] = reflect.ValueOf(args[i])
-	}
-
-	return reflect.ValueOf(obj).MethodByName(name).Call(inputs)
-}
-
-func RouteHandle(ctx *gin.Context, obj any, method string) {
-	rf := Invoke(obj, method, ctx)
-	res := rf[0].Interface().(string)
-
-	ctx.Data(http.StatusOK, "text/html; charset=utf-8", []byte(res))
-}
-
 func main() {
 
+	gr := goreact.GoReact{}
+	gr.Init()
 	r := gin.Default()
 	index := &controller.IndexController{}
+	fmt.Printf("%d\n", index)
 	user := &controller.UserController{}
 	r.GET("/", func(ctx *gin.Context) {
-		RouteHandle(ctx, index, "Index")
+		gr.HandleRoute(ctx, index, "Index")
 	})
 
 	r.GET("/login", func(ctx *gin.Context) {
-		RouteHandle(ctx, user, "Login")
+		gr.HandleRoute(ctx, user, "Login")
 	})
 
-	r.POST("/login", func(ctx *gin.Context) {
-
-	})
+	r.POST("/login", user.LoginPost)
 
 	r.Run(":8090")
 
---
+++ src/goreact.go
@@ -0,0 +1,69 @@
+package goreact
+
+import (
+	"net/http"
+	"reflect"
+
+	"github.com/gin-gonic/gin"
+)
+
+type GoReact struct {
+	Data map[string]map[string]string
+}
+
+func (gr *GoReact) Init() {
+	gr.Data = make(map[string]map[string]string)
+}
+func Invoke(obj any, name string, args ...any) []reflect.Value {
+	inputs := make([]reflect.Value, len(args))
+	for i, _ := range args {
+		inputs[i] = reflect.ValueOf(args[i])
+	}
+
+	return reflect.ValueOf(obj).MethodByName(name).Call(inputs)
+}
+
+func (gr *GoReact) getCache(obj string, method string) string {
+	return gr.Data[obj][method]
+}
+
+func (gr *GoReact) checkCache(obj string, method string) bool {
+	if _, ok := gr.Data[obj]; ok {
+		if _, ok1 := gr.Data[obj][method]; ok1 {
+			return true
+		}
+	}
+	return false
+}
+
+func (gr *GoReact) cache(obj string, method string, data string) string {
+	if gr.Data[obj] == nil {
+		gr.Data[obj] = make(map[string]string)
+	}
+
+	gr.Data[obj][method] = data
+	return data
+}
+
+func getType(myvar interface{}) string {
+	if t := reflect.TypeOf(myvar); t.Kind() == reflect.Ptr {
+		return "*" + t.Elem().Name()
+	} else {
+		return t.Name()
+	}
+}
+
+func (gr *GoReact) HandleRoute(ctx *gin.Context, obj any, method string) {
+	cname := getType(obj)
+	res := ""
+	if gr.checkCache(cname, method) {
+		res = gr.getCache(cname, method)
+
+	} else {
+		rf := Invoke(obj, method, ctx)
+		res = rf[0].Interface().(string)
+		gr.cache(cname, method, res)
+	}
+
+	ctx.Data(http.StatusOK, "text/html; charset=utf-8", []byte(res))
+}
